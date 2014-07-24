eb59d178b7185184d4ae4f480a79843bec5518cf
Paul Stamy <pstamy@cropquest.com>
2014-07-24 09:45:49 -0500

    Finished 'First Homework' Added User scaffold, associated it with Projects and Tickets and added select boxes to add user to tickets and projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
   has_many :tickets, :dependent => :destroy
+  belongs_to :user
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ActiveRecord::Base
   validates_presence_of :name, :comment
 
   belongs_to :project
+  belongs_to :user
 end
