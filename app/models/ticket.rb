445019ca7f1dc34239d1d7a3c9deeb86e96a0885
Paul Stamy <pstamy@cropquest.com>
2014-07-24 10:17:28 -0500

    Scaffolded IssueType and IssueStatus and set associations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ActiveRecord::Base
   validates_presence_of :name, :comment
-
+  has_many :types
+  has_many :statuses
   belongs_to :project
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :issuestatuses
+
+  resources :issuetypes
+
   resources :users
 
   resources :projects
