13dc749f689b6a00eac210a10cf8cabcd99d025d
Paul Stamy <pstamy@cropquest.com>
2014-07-24 10:41:41 -0500

    Added select boxes for Issue Type and Issue Status to ticket form

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ActiveRecord::Base
   validates_presence_of :name, :comment
-  has_many :types
-  has_many :statuses
+  has_many :issue_types
+  has_many :issue_status
   belongs_to :project
   belongs_to :user
 end
